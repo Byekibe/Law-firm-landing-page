908fa4faba5d777fa45382add05b753b13e98f46
Byekibe <kibetpeter95@gmail.com>
2024-03-12 01:32:25 +0300

    Add About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center font-bold text-gray-500">About Us</h1>
+      <p className="p-4 text-lg ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore harum
+        porro illum officia dolores. Vero perferendis quam a cum sed eligendi
+        labore quibusdam nostrum reprehenderit odit et eos explicabo nulla
+        officiis ducimus, rem reiciendis at recusandae hic rerum eveniet sequi.
+      </p>
+    </div>
+  );
+}
