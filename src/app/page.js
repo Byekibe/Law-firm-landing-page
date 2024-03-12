97e7136ef5248637becc81954a0158e128be59ce
Byekibe <kibetpeter95@gmail.com>
2024-03-12 19:20:12 +0300

    Add home directory

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
+import Header from "@/components/header/Header";
 import Image from "next/image";
+import Hero from "@/components/hero/Hero";
+import MainContent from "@/components/mainSection/Main";
+import About from "../components/about/About.jsx";
+import Testimonial from "@/components/testimonial/Testimonial.jsx";
+import Footer from "@/components/footer/Footer.jsx";
 
 export default function Home() {
   return (
     <main className="">
-      <h2>Law Firm Landing page</h2>
+      <Hero />
+      <MainContent />
+      <About />
+      <Testimonial />
+      <Footer />
     </main>
   );
 }
